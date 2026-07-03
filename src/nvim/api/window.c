f7cbcf11bc977410173d1939e53f2933e78589a6
XiaowenHu96 <me@xiaowenhu.com>
2026-07-03 21:10:50 +0800

    fix: guard calls with TRY_WRAP for future proofing

--- src/nvim/api/window.c
+++ src/nvim/api/window.c
@@ -596,9 +596,9 @@ void nvim_win_resize(Window win, Integer height, Integer width, Dict(win_resize)
                  opts->anchor.data, { return; });
   }
   if (height > 0) {
-    win_setheight_win((int)height, w, from_top);
+    TRY_WRAP(err, win_setheight_win((int)height, w, from_top));
   }
-  if (width > 0) {
-    win_setwidth_win((int)width, w, from_left);
+  if (!ERROR_SET(err) && width > 0) {
+    TRY_WRAP(err, win_setwidth_win((int)width, w, from_left));
   }
 }
